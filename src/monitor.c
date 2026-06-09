dc7a7f6ddb52ec83b25901a196974d6bd4e6f909
Popoliku <andres1239cl@gmail.com>
2026-06-10 01:33:21 +0200

    feature: add interactive command prompt and input parsing

--- src/monitor.c
+++ src/monitor.c
@@ -4,6 +4,31 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <string.h>
+
+char** get_input(FILE* stream, char** comando, int* pmon_argc) {
+	size_t capacidad = 0;
+	if(getline(comando, &capacidad, stream) == -1 && !feof(stream)) {
+		perror("Error leyendo entrada");
+	}
+	char* ptr = strchr(*comando, '\n');
+	*ptr = 0;
+	capacidad = 10;
+	int argc = 0;
+	char** argv = malloc(capacidad*sizeof(char*)); 
+	char* token = strtok(*comando, " ");
+	while(token != NULL) {
+		if(argc == (int)capacidad) {
+			capacidad *= 2;
+			argv = realloc(argv, capacidad*sizeof(char*));
+		}
+		argv[argc++] = token;
+		token = strtok(NULL, " ");
+	}
+	argv[argc] = NULL;
+	*pmon_argc = argc;
+	return argv;
+}
 
 int main(int argc, char** argv) {
 	if(argc < 2) {
@@ -24,6 +49,18 @@ int main(int argc, char** argv) {
 	waitpid(pid, &status, 0);
 	ptrace(PTRACE_SETOPTIONS, pid, NULL, PTRACE_O_TRACEFORK);
 	while(1) {
+		printf("pmon> ");
+		fflush(stdout);
+		char* comando = NULL;
+		int pmon_argc = 0;
+		char** pmon_argv = get_input(stdin, &comando, &pmon_argc);
+		for(int i = 0; i<pmon_argc; i++) {
+			printf("%s ", pmon_argv[i]); 
+		}
+		printf("\n");
+		if(pmon_argc > 2) printf("Invalid number of arguments\n");
+		free(pmon_argv);
+		free(comando);
 		//ptrace(PTRACE_SYSCALL, pid, NULL, NULL);
 	}
 }
